640295ee56e0be6decd401199ba53e5a50efcb15
Antonio Jefferson <antoniojeferson96@gmail.com>
2022-01-15 13:10:50 -0300

    refactor(pokeList.dart) Melhorando animaçoes e removendo codigos desnecessarios

--- lib/widgets/pokeList.dart
+++ lib/widgets/pokeList.dart
@@ -1,30 +1,34 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_staggered_animations/flutter_staggered_animations.dart';
 import 'package:pokedex/models/pokemon.dart';
 import 'package:pokedex/widgets/pokeCard.dart';
 
 class PokeList extends StatelessWidget {
-  final double? width;
-  final double? height;
   final List<Pokemon> pokemons;
-  const PokeList({ Key? key, this.height, required this.pokemons, this.width }) : super(key: key);
+  const PokeList({ Key? key, required this.pokemons}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      width: width,
-      height: height,
-      // color: Colors.blue,
+    return AnimationLimiter(
       child: GridView.builder(
         gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
         ),
         padding: const EdgeInsets.symmetric(horizontal: 20),
         itemCount: pokemons.length,
+        physics: const BouncingScrollPhysics(),
         itemBuilder: (context, index) {
-          return Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: PokeCard(
-              pokemon: pokemons[index]
+          return AnimationConfiguration.staggeredGrid(
+            position: index,
+            duration: const Duration(milliseconds: 375),
+            columnCount: 2,
+            child: ScaleAnimation(
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: PokeCard(
+                  pokemon: pokemons[index]
+                ),
+              ),
             ),
           );
         }
